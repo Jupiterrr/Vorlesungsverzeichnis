20d6f07304850c17ce0e9d366036c19fec7ad7c3
Carsten Griesheimer <hallo@carstengriesheimer.de>
2013-06-05 14:26:34 +0200

    Removes page caching from map search

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,7 +1,6 @@
 class MapController < ApplicationController
 
   caches_action :index, :show, :list, :layout => false
-  caches_page :search
 
   def search
     result = Poi.search(params[:q]).map(&:to_search_json)
@@ -12,8 +11,8 @@ class MapController < ApplicationController
     colors = ["#4499ff", "#324189", "#730F5B", "#F52410", "#188D98", "#90B127", "#3165C9", "#2A9C6C", "#F67710"]
     @pois = Poi.all(:include => :poi_groups)
     @poi = Poi.find_by_id(params[:id])
-    
-    @js_pois = @pois.map do |poi| 
+
+    @js_pois = @pois.map do |poi|
       data = poi.as_json.slice("id", "name", "lat", "lng", "building_no", "address")
       data["poi_group_ids"] = poi.poi_group_ids
       data
