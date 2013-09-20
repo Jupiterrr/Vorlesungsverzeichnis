2323651b6bad8c45bef4fba30c6d3ada61a21f13
Carsten Griesheimer <hallo@carstengriesheimer.de>
2013-09-20 16:58:42 +0200

    Place url in description bacause that's how I roll

--- app/models/timetable.rb
+++ app/models/timetable.rb
@@ -34,21 +34,26 @@ class Timetable
     user = User.find_by_timetable_id timetable_id
     raise ActiveRecord::RecordNotFound if user.nil?
     events = user.events
-    RiCal.Calendar do |cal|
+    ical = RiCal.Calendar do |cal|
+      cal.add_x_property 'X-WR-CALNAME', "KitHub.de"
       events.each do |event|
         event.dates.each do |date|
           cal.event do |e|
+            event_url = Rails.application.routes.url_helpers.event_url(event, host: "www.kithub.de")
             e.summary     = event.name
-            #event.description = "First US Manned Spaceflight\n(NASA Code: Mercury 13/Friendship 7)"
             e.dtstart     = date.start_time
             e.dtend       = date.end_time
             e.location    = date.room || ""
-            e.url         = Rails.application.routes.url_helpers.event_url(event, host: "www.kithub.de")
+            e.description = event_url
+            e.url         = event_url
             e.uid         = "#{date.id}@kit.edu"
           end
         end
       end
     end
+    # rical puts symbols instead of strings in the file
+    # so we remove the prepending colon
+    output = ical.to_s.gsub("X-WR-CALNAME::", "X-WR-CALNAME:")
   end
 
 end
