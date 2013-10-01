5441e8480f8c10ec8760a1cc031d7aeb45be7b7b
Carsten Griesheimer <hallo@carstengriesheimer.de>
2013-10-01 15:00:33 +0200

    Change index page for logged in users to vvz

--- config/routes.rb
+++ config/routes.rb
@@ -75,7 +75,7 @@ KITBox::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'dashboard#index', :constraints => RoleConstraint.new(:user)
+  # root :to => 'dashboard#index', :constraints => RoleConstraint.new(:user)
   root :to => redirect("/vvz")
 
   # See how all your routes lay out with "rake routes"
