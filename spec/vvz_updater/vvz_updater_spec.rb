0a3c2af28d94a4d3c92b2806e096f821748e9ee4
Carsten Griesheimer <hallo@carstengriesheimer.de>
2013-11-22 09:08:31 +0100

    Remove

--- spec/vvz_updater/vvz_updater_spec.rb
+++ spec/vvz_updater/vvz_updater_spec.rb
@@ -3,7 +3,7 @@ require "vvz_updater/vvz_updater"
 
 describe VVZUpdater::EventUpdater, vcr: {match_requests_on: [:body, :uri]} do
 
-  describe ".update_event" do
+  describe ".update_event", no_ci: true do
     it "updates correctly" do
       db_event = Event.create! external_id: "0xaefda07be09a7c43875d5ce13378a1cc"
       updater = VVZUpdater.update_event(db_event, false)
