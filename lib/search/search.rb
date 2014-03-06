d2a0f094ed93d550f3011d618611547160bf8490
Carsten Griesheimer <hallo@carstengriesheimer.de>
2014-03-07 00:37:02 +0100

    Add event no search

--- lib/search/search.rb
+++ lib/search/search.rb
@@ -2,19 +2,20 @@ class Search
 
   def initialize(params)
     @page = params.fetch(:p, 0)
-    @query = params.fetch(:q, "")
+    @query = params.fetch(:q, "").strip
 
     term_id = params.fetch(:term, Vvz.current_term.id)
     @term = Vvz.find(term_id)
   end
 
 
-  SearchResult = Struct.new(:query, :term, :vvzs, :events)
+  SearchResult = Struct.new(:query, :term, :vvzs, :events, :event_no_match)
 
   def search()
     vvz_results = search_vvz(@query, @term, @page)
     event_results = search_event_paged(@query, @term, @page)
-    SearchResult.new(@query, @term, vvz_results, event_results)
+    event_no_match = search_event_no(@query, @term, @page).first
+    SearchResult.new(@query, @term, vvz_results, event_results, event_no_match)
   end
 
 
@@ -36,17 +37,24 @@ class Search
   end
 
   def search_event(query, term)
-    Event.vvz_search(query).where(term: term.name).not_orphans
+    Event.vvz_search(query).where(term: term.name).not_orphans.uniq
   end
 
   def search_event_paged(query, term, page)
     search_event(query, term).page(page).per(10)
   end
 
+  def search_event_no(query, term, page=0)
+    if page == 0
+      Event.where(term: term.name).not_orphans.find_by_no(query).limit(1)
+    end
+  end
+
   def autocomplete_search(query, term)
     vvz_results = search_vvz(query, term).limit(10)
     event_results = search_event(query, term).limit(10)
-    results = vvz_results + event_results
+    event_no_match = search_event_no(@query, @term, @page)
+    results = vvz_results + event_results + event_no_match
     js_result(results)
   end
 
