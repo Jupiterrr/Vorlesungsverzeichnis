287a3e88f71834b942d3586fe8c05125a49bf916
Carsten Griesheimer <hallo@carstengriesheimer.de>
2014-06-21 17:21:01 +0200

    Add events api params

--- app/api/api_v1/terms.rb
+++ app/api/api_v1/terms.rb
@@ -1,6 +1,6 @@
 class API_v1::Terms < Grape::API
 
-  resource :terms do    
+  resource :terms do
 
 
     desc "List available terms"
@@ -8,7 +8,7 @@ class API_v1::Terms < Grape::API
       Vvz.terms.map {|term| TermPresenter.new(term).as_json }
     end
 
-    
+
     desc "List nodes for a term."
     params do
       requires :id, type: Integer, desc: "Term id."
@@ -19,7 +19,7 @@ class API_v1::Terms < Grape::API
       nodes = term.descendants
       result = nodes.map {|vvz| VvzPresenter.new(vvz).as_json }
 
-      term.leafs.includes(:events).each do |vvz| 
+      term.leafs.includes(:events).each do |vvz|
         vvz.events.each do |event|
           result << VvzEventPresenter.new(event, vvz).as_json(event_type: params[:event_type])
         end
@@ -27,17 +27,19 @@ class API_v1::Terms < Grape::API
       result
     end
 
-    
+
     desc "List events for a term"
     params do
       requires :id, type: Integer, desc: "Term id."
       optional :type, type: String, desc: "[default|detail]"
+      optional :no, :type => Integer, :desc => "Lehrveranstaltungsnummer"
     end
     get ':id/events' do
       term = Vvz.find(params[:id])
       events = Event.where(term: term.name)
+      events = events.where(no: params[:no].to_s) if params[:no]
       presenter = params[:type] == "detail" ? EventPresenter : TermEventPresenter
-      events.map {|event| presenter.new(event).as_json(no_dates: true) }
+      events.map {|event| presenter.new(event).as_json(no_dates: false) }
     end
 
 
